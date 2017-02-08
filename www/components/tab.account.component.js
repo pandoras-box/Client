9a01362d5019c178f1ee52f136d80df2a8b361d8
Guillermo Moratorio <moratorio.guillermo@gmail.com>
2017-02-08 14:55:28 -0500

    half-refactored boilerplate

---
+++ www/components/tab.account.component.js
@@ -0,0 +1,30 @@
+// Intentionally commented out to revisit later
+//
+// (function() {
+//   'use strict';
+//   angular.module('pandoras-box')
+//   .component('tab-account', {
+//     controller:controller,
+//     templateUrl: './components/tab-account.html'
+//   })
+//
+//
+//
+//   function controller(){
+//     const vm = this;
+//
+//     vm.settings = {
+//       enableFriends: true
+//     };
+//
+//     vm.$onInit = function(){
+//       console.log("In tab/account!");
+//
+//     }
+//
+//   }
+//
+//
+//
+//
+// }());
---
+++ www/components/tab.dash.component.js
@@ -0,0 +1,23 @@
+// Intentionally commented out to revisit later
+//
+//
+// (function() {
+//   'use strict';
+//   angular.module('pandoras-box')
+//   .component('tab-dash', {
+//     controller:controller,
+//     templateUrl: './components/tab-dash.html'
+//   })
+//
+//   function controller(){
+//     const vm = this;
+//
+//     vm.$onInit = function(){
+//       console.log("In tabs/dash!");
+//
+//     }
+//
+//   }
+//
+//
+// }());
