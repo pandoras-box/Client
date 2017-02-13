339ffa654039fa75f2c58040f1f45e7773b8f55b
Guillermo Moratorio <moratorio.guillermo@gmail.com>
2017-02-13 14:55:19 -0700

    added first pass at facebook oauth

---
+++ platforms/browser/www/components/tab.tasks.component.js
@@ -0,0 +1,43 @@
+// Intentionally commented out to revisit later
+//
+// (function() {
+//   'use strict';
+//   angular.module('pandoras-box')
+//   .component('tab-chats', {
+//     controller:controller,
+//     templateUrl: './components/tab-chats.html'
+//   })
+//
+//   function controller(Chats){
+//     const vm = this;
+//
+//     vm.chats = Chats.all();
+//     vm.remove = function(chat) {
+//       Chats.remove(chat);
+//     }
+//
+//     vm.$onInit = function(){
+//       console.log("In tabs/chats!");
+//
+//     }
+//
+//   }
+//
+//
+//   // .controller('ChatsCtrl', function($scope, Chats) {
+//   //   // With the new view caching in Ionic, Controllers are only called
+//   //   // when they are recreated or on app start, instead of every page change.
+//   //   // To listen for when this page is active (for example, to refresh data),
+//   //   // listen for the $ionicView.enter event:
+//   //   //
+//   //   //$scope.$on('$ionicView.enter', function(e) {
+//   //   //});
+//   //
+//   //   $scope.chats = Chats.all();
+//   //   $scope.remove = function(chat) {
+//   //     Chats.remove(chat);
+//   //   };
+//   // })
+//
+//
+// }());
