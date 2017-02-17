e4e091a80447105a323f947634ac5ca3c4020d52
Guillermo Moratorio <moratorio.guillermo@gmail.com>
2017-02-16 19:52:00 -0700

    route for updateTask complete

--- platforms/ios/www/components/services.js
+++ platforms/ios/www/components/services.js
@@ -5,8 +5,8 @@
 
 
 
-    const SERVER_URL = `https://pandoras-box-team.herokuapp.com`;
-    //     const SERVER_URL = `http://10.6.66.4:5000`;
+    // const SERVER_URL = `https://pandoras-box-team.herokuapp.com`;
+        const SERVER_URL = 'http://10.6.65.77:5000';
     // const SERVER_URL = `http://192.168.0.5:5000`;
     // const SERVER_URL = `http://10.6.65.77:5000`;
 
@@ -95,6 +95,13 @@
                 userToken,
                 tempTasks
             });
+        }
+
+        this.updateTaskStatus = function(userToken, currentTask) {
+          return $http.post(`${SERVER_URL}/secure/update-task-status`, {
+              userToken,
+              currentTask
+          });
         }
 
         // this.getSingleTask = function(batchEventId) {
